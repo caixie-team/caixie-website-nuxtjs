05bfba7109b0ee91127eef9b6d9626dd35c0c15b
baisheng <baisheng@gmail.com>
2020-02-10 13:15:49 +0800

    update nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: '北京采撷科技有限公司',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
