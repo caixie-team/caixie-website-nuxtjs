2eec293788abfd70287b284f0b4e6ea98fe8de6e
baisheng <baisheng@gmail.com>
2020-02-10 22:04:03 +0800

    update readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -222,11 +222,7 @@ module.exports = {
     boxShadow: {
       default: '0 0 0 1px rgba(20, 11, 47, 0.12), 0 4px 4px -4px rgba(0, 0, 0, .32)',
       md: BOX_SHADOW_BUTTON_HOVER,
-      // md: '0 0 0 1px rgba(20, 11, 47, 0.32), 0 4px 4px -4px rgba(0, 0, 0, .32)',
-      // md: '0 0 0 1px rgba(20, 11, 47, 0.32), 0 4px 4px -4px rgba(0, 0, 0, .32)',
-      // md: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
       lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
-      // default: `${BOX_SHADOW_BORDER}, ${BOX_SHADOW_SITTING}`,
       xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
       '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
@@ -239,7 +235,6 @@ module.exports = {
       lifted: BOX_SHADOW_LIFTED,
       large: BOX_SHADOW_LARGE,
       focus: BOX_SHADOW_FOCUS,
-      // button: BOX_SHADOW_BUTTON,
       buttonHover: BOX_SHADOW_BUTTON_HOVER
     },
     container: {},
